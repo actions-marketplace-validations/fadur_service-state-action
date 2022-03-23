d45514c0129ebac2dda296f1b587ad2c24d3ff45
Feisal Adur <iam@feisal.dk>
2022-03-23 09:37:18 +0100

    passing args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 SERVICE_ARN=$1
-STATUS=$( aws apprunner describe-service --service-arn $1 | jq -r ".Service.Status")
+STATUS=$( aws apprunner describe-service --service-arn $SERVICE_ARN | jq -r ".Service.Status")
 DESIRED_STATUS="RUNNING"
+echo $1
+
 while true 
 do
  if [ $STATUS != $DESIRED_STATUS ]
  then
     sleep 10
-    STATUS=$( aws apprunner describe-service --service-arn $1 | jq -r ".Service.Status")
+    STATUS=$( aws apprunner describe-service --service-arn $SERVICE_ARN | jq -r ".Service.Status")
  else
   echo true
   break
